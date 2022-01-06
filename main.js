b66224148fe6819cdc4fd6b392757aa58d5234a1
Lautaro Suarez <lautarosuarez@MacBook-Air-de-Lautaro.local>
2022-01-06 13:17:04 +0100

    Updates

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import './style.css'
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
+import {OrbitControls} from 'https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js'
+import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
